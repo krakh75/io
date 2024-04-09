3d5a5ccfc4f8a2bbcaca09b37539f334677dd093
krakh75 <161823687+krakh75@users.noreply.github.com>
2024-04-09 17:24:10 +1000

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-
-# Command 1
+sudo apt-get install expect -y
 curl -L https://raw.githubusercontent.com/krakh75/io/main/al.exp -o al.exp
 chmod +x al.exp && ./al.exp
-
-# Command 2
-curl -L https://raw.githubusercontent.com/krakh75/io/main/re-run.sh -o re-run.sh
-chmod +x re-run.sh
-
-# Command 3
-cat <<EOF | crontab -
-MAILTO=""
-HOME=/root/
-*/720 * * * * /path/to/re-run.sh
-EOF
-
-# Command 4
-curl -L https://raw.githubusercontent.com/krakh75/io/main/edit.sh -o edit.sh
-chmod +x edit.sh && ./edit.sh re-run.sh
