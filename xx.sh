52a315c419059162cabaea78b1c198ed97478f50
krakh75 <161823687+krakh75@users.noreply.github.com>
2024-04-09 19:19:53 +1000

    Update xx.sh

--- xx.sh
+++ xx.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-curl -L https://raw.githubusercontent.com/krakh75/io/main/re-run.sh -o re-run.sh && chmod +x ./re-run.sh && \
-curl -L https://raw.githubusercontent.com/krakh75/io/main/edit.sh -o edit.sh && chmod +x ./edit.sh && \
+
+# Download re-run.sh
+curl -L https://raw.githubusercontent.com/krakh75/io/main/re-run.sh -o re-run.sh
+# Make re-run.sh executable
+chmod +x re-run.sh
+
+# Download edit.sh
+curl -L https://raw.githubusercontent.com/krakh75/io/main/edit.sh -o edit.sh
+# Make edit.sh executable
+chmod +x edit.sh
+
+# Run edit.sh with re-run.sh as an argument
 ./edit.sh re-run.sh
